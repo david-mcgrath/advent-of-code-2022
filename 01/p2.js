8034bc6fcf0dd5add94cb2c85f39ee2d786e9fd0
David McGrath <david.mcgrath13@gmail.com>
2022-12-01 13:23:48 +0800

    Added day 1
    Relaxed intro to input parsing

---
+++ 01/p2.js
@@ -0,0 +1,38 @@
+var url = "https://adventofcode.com/2022/day/1/input";
+var testData = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+function getInput(isTest) {
+	return isTest
+		? new Promise((resolve, reject) => resolve(testData))
+		: fetch(url).then(res => res.text());
+}
+
+await getInput(false)
+	.then(input => {
+		let elves = input
+			.split("\n\n")
+            .map(x => x
+                .split("\n")
+                .filter(y => y)
+                .map(y => +y))
+			.filter(x => x.length > 0);
+        return elves
+            .map(x => x
+                 .reduce((a, b) => a + b))
+            .sort((a, b) => b - a)
+            .slice(0, 3)
+            .reduce((a, b) => a + b);
+	});
